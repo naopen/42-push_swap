82480885fe97e5f444f895d72d21507ffadac70d
naopen <naokizssm@gmail.com>
2024-05-01 12:06:26 +0900

    Update error handling functions

--- srcs/errors.c
+++ srcs/errors.c
@@ -6,29 +6,19 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 04:19:12 by nkannan           #+#    #+#             */
-/*   Updated: 2024/05/01 04:19:31 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/05/01 12:05:45 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	validate_args(int argc, char **argv)
+void	args_error(void)
 {
-	int		i;
-	int		j;
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
+}
 
-	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		if (ft_strlen(argv[i]) == 0)
-			exit_error();
-		while (argv[i][j])
-		{
-			if (!ft_isdigit(argv[i][j]) && argv[i][j] != '-')
-				exit_error();
-			j++;
-		}
-		i++;
-	}
+void	malloc_error(void)
+{
+	exit(1);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,8 +6,68 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 04:02:15 by nkannan           #+#    #+#             */
-/*   Updated: 2024/05/01 04:06:41 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/05/01 12:05:13 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	validate_args(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		if (ft_strlen(argv[i]) == 0)
+			args_error();
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]) && argv[i][j] != '-')
+				args_error();
+			j++;
+		}
+		i++;
+	}
+}
+
+void	parse_args(t_data *data, int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	int		value;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			value = ft_atoi(&argv[i][j]);
+			if (value > INT_MAX || value < INT_MIN)
+				args_error();
+			push_a(&data->a, value);
+			while (ft_isdigit(argv[i][j]) || argv[i][j] == '-')
+				j++;
+			while (argv[i][j] && !ft_isdigit(argv[i][j]) && argv[i][j] != '-')
+				j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_data	*data;
+
+	if (argc == 1)
+		return (0);
+	validate_args(argc, argv);
+	data = data_init(data);
+	parse_args(data, argc, argv);
+	radix_sort(&data->a, &data->b);
+	free_data(data);
+	return (0);
+}
